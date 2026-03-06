7eb8d9c8e758f99eef235f967e1b469f71da19b2
Sineth Wickramaratna <sinethwickramaratna@gmail.com>
2026-03-06 21:38:55 +0530

    feat: update Dockerfile and docker-compose for improved environment configuration and MongoDB connection

--- Server/config/db.js
+++ Server/config/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb://127.0.0.1:27017/suvadhari_db');
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
